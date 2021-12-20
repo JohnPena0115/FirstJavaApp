e782f0df91573e1fa7c0bcbfcc3e3a2184dd21eb
JohnPena0115 <calmwinds06@gmail.com>
2021-12-20 13:54:14 -0500

    Uploading getFirstIndexValue Java Algo

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,12 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println(getFirstIndexValue(new int[] {3,1, 4}));
+    }
+
+    public static int getFirstIndexValue(int[] arr) {
+
+        return arr[0];
     }
 }
