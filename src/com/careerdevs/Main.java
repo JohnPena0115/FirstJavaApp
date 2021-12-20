a5d5ac221b98b5dfa253df8dab0c2270f89a0e5d
JohnPena0115 <calmwinds06@gmail.com>
2021-12-20 17:00:00 -0500

    Uploading search Java algo

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println(getFirstIndexValue(new int[] {3,1, 4}));
 
         System.out.println(getNextNum(6));
+
+        System.out.println(search(new int[] {2,3,7},3));
     }
 
     public static int getFirstIndexValue(int[] arr) {
@@ -17,5 +19,18 @@ public class Main {
     public static int getNextNum(int num) {
 
         return ++num;
+    }
+
+    public static int search(int arr[], int item){
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == item) {
+
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
