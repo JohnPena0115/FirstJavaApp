4b36bade87185479ecd5c16f534ab36ef93bb89a
JohnPena0115 <calmwinds06@gmail.com>
2021-12-20 15:32:53 -0500

    Uploading getNextNum Java Algo

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,10 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(getFirstIndexValue(new int[] {3,1, 4}));
+
+        System.out.println(getNextNum(6));
     }
 
     public static int getFirstIndexValue(int[] arr) {
 
         return arr[0];
+    }
+
+    public static int getNextNum(int num) {
+
+        return ++num;
     }
 }
